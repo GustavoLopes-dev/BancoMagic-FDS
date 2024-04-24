eb40b4d9a24e205e509c2093e48d71bd4e04335c
Gustavo Lopes <80708475+GustavoLopes-dev@users.noreply.github.com>
2024-04-24 17:45:05 -0300

    Integra com a Brasil API

--- BancoMagic/BancoMagic/src/main/java/com/example/BancoMagic/Controller.java
+++ BancoMagic/BancoMagic/src/main/java/com/example/BancoMagic/Controller.java
@@ -1,8 +1,8 @@
 package com.example.BancoMagic;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -10,8 +10,12 @@ import java.util.List;
 public class Controller {
     private ImplementsBanco bancoContas;
 
+    private final RestTemplate restTemplate;
+
+
     public Controller(ImplementsBanco bancoContas) {
         this.bancoContas = bancoContas;
+        this.restTemplate = new RestTemplate();
     }
 
     @GetMapping("")
@@ -30,6 +34,33 @@ public class Controller {
     @CrossOrigin(origins = "*")
     public List<Cliente> getListaClientes() {
         return bancoContas.getClientes();
+    }
+
+    @GetMapping("/clientesSize")
+    @CrossOrigin(origins = "*")
+    public int getTotalClientes() {
+        return bancoContas.quantidadeClientes();
+    }
+
+    @PostMapping("/clientesCadastro")
+    @CrossOrigin(origins = "*")
+    public boolean setCadastraCliente(@RequestBody Cliente novo) {
+        bancoContas.adicionarCliente(novo);
+        return true; // ou alguma lógica para indicar se o cliente foi cadastrado com sucesso
+    }
+
+    @GetMapping("/teste-cep")
+    @CrossOrigin
+    public Object consultaCep(){
+        String url = "https://brasilapi.com.br/api/cep/v1/" + "91450030";
+        return restTemplate.getForObject(url, Object.class);
+    }
+
+    @GetMapping("/teste-cnpj")
+    @CrossOrigin
+    public Object consultaCNPJ(){
+        String url = "https://brasilapi.com.br/api/cnpj/v1/" + "35337331000104";
+        return restTemplate.getForObject(url, CNPJ.class);
     }
 
 
